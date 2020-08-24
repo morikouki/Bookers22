63adbd1a3f5955024af00cff7335afd0b9468df7
vagrant <akzouksia@icloud.com>
2020-08-24 08:38:44 +0000

    [Finish]ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,23 +1,27 @@
 class BookCommentsController < ApplicationController
 
+	before_action :ensure_correct_user, only: [:create, :destroy]
+
 	def create
 		book = Book.find(params[:book_id])
 		comment = current_user.book_comments.new(book_comment_params)
 		comment.book_id = book.id
 	    comment.save
-		redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
 		book = Book.find(params[:book_id])
 		comment = current_user.book_comments.find_by(params[book_id: book.id])
 		comment.destroy
-		redirect_back(fallback_location: root_path)
 	end
 
 	private
 
 	def book_comment_params
 		params.require(:book_comment).permit(:comment)
+	end
+
+	def ensure_correct_user
+		@book = Book.find(params[:book_id])
 	end
 end
